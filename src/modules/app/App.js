808744b4a5ff79827e5a7cc44328dfe32a00661e
= <terrymorrow@gmail.com>
2018-04-17 18:01:59 -0400

    add introduction module

---
+++ src/modules/app/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Introduction from '../introduction/Introduction'
+import './App.css';
+
+class App extends Component {
+  render = ()=> (
+    <div className="App">
+      <Introduction />
+    </div>
+  )
+}
+
+export default App;
